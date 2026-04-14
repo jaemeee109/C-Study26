6054d9a87b67b891ea0a3cf2a5f114461f262a1c
DESKTOP-VP0J4D4\LeeJaeEun <jae_24@naver.com>
2026-04-14 23:42:37 +0900

    C언어 학습시작

---
+++ 01HelloWolrd.c
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+int main(void) {
+	// printf("Hello World")
+	// printf("Hello World\n")
+	int num01, num02;
+	printf("첫 번째 정수를 입력하세요: ");
+	scanf("%d", &num01);
+	printf("두 번째 정수를 입력하세요: ");
+	scanf("%d", &num02);
+	printf("입력하신 두 정수의 합은 %d입니다.\n", num01 + num02);
+	return 0;
+}
+
